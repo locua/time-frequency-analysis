a06790860d2f48c5e5bce080bfcf851b7008bf05
Louis <poes997@gmail.com>
2023-07-06 17:01:07 +0100

    latest

--- rename_events.py
+++ rename_events.py
@@ -2,11 +2,9 @@ import mne
 import pandas as pd
 import pybv
 
-def test():
-    print('Hello!')
-
 def relabel_events(session_id, pos_id):
-    print('Data file: ', session_id + '/'+session_id+'_'+pos_id+'.vhdr')
-    raw = mne.io.read_raw_brainvision(session_id + '/'+session_id+'_'+pos_id+'.vhdr', preload=True)   
+    vhdr_file = session_id + '/'+session_id+'_'+pos_id+'.vhdr'
+    print('Data file: ', vhdr_file)
+    raw = mne.io.read_raw_brainvision(vhdr_file, preload=True)
 
-    pass
+
